9eed54a8a471e0b4044d209f6e4bafa1dda670e5
Achilles-Dev <solohag92@gmail.com>
2023-10-16 21:01:02 +0000

    Update database queries

--- app/api/journal/route.ts
+++ app/api/journal/route.ts
@@ -1,28 +1,43 @@
-import { analyze } from "@/utils/ai"
-import { getUserFromClerkID } from "@/utils/auth"
-import { prisma } from "@/utils/db"
-import { revalidatePath } from "next/cache"
-import { NextResponse } from "next/server"
+import { db } from '@/drizzle/db'
+import { analysis, journalEntries } from '@/drizzle/schema'
+import { analyze } from '@/utils/ai'
+import { getUserFromClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+import { eq } from 'drizzle-orm'
+import { revalidatePath } from 'next/cache'
+import { NextResponse } from 'next/server'
 
 export const POST = async () => {
-    const user = await getUserFromClerkID()
-    const entry = await prisma.journalEntry.create({
-        data: {
-            userId: user.id,
-            content: 'Write about your daily mood!',
-        }
-    })
+  const user = await getUserFromClerkID()
+  await db
+    .insert(journalEntries)
+    .values({ userId: user[0].id, content: 'Write about your daily mood!' })
+  // const entry = await prisma.journalEntry.create({
+  //     data: {
+  //         userId: user.id,
+  //         content: 'Write about your daily mood!',
+  //     }
+  // })
 
-    const analysis = await analyze(entry.content)
-    await prisma.analysis.create({
-        data: {
-            userId: user.id,
-            entryId: entry.id,
-            ...analysis
-        }
-    })
+  const entry = await db
+    .select()
+    .from(journalEntries)
+    .where(eq(journalEntries.userId, user[0].id))
+    .where(eq(journalEntries.content, 'Write about your daily mood!'))
 
-    revalidatePath('/journal')
+  const myAnalysis = await analyze(entry[0].content)
+  //   await prisma.analysis.create({
+  //     data: {
+  //       userId: user.id,
+  //       entryId: entry.id,
+  //       ...analysis,
+  //     },
+  //   })
+  await db
+    .insert(analysis)
+    .values({ userId: user[0].id, entryId: entry[0].id, ...myAnalysis })
 
-    return NextResponse.json({ data: entry})
+  revalidatePath('/journal')
+
+  return NextResponse.json({ data: entry })
 }
--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,24 +1,38 @@
+import { db } from '@/drizzle/db'
+import { users } from '@/drizzle/schema'
 import { prisma } from '@/utils/db'
 import { currentUser } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm'
 import { redirect } from 'next/navigation'
 
 const createNewUser = async () => {
   const user = await currentUser()
   console.log(user)
 
-  const match = await prisma.user.findUnique({
-    where: {
-      clerkId: user.id as string,
-    },
-  })
+  // const match = await prisma.user.findUnique({
+  //   where: {
+  //     clerkId: user.id as string,
+  //   },
+  // })
+  const match = await db
+    .select()
+    .from(users)
+    .where(eq(users.clerkId, user[0].id))
 
   if (!match) {
-    await prisma.user.create({
-      data: {
-        clerkId: user.id,
-        email: user?.emailAddresses[0].emailAddress,
-      },
-    })
+    // await prisma.user.create({
+    //   data: {
+    //     clerkId: user.id,
+    //     email: user?.emailAddresses[0].emailAddress,
+    //   },
+    // })
+
+    await db
+      .insert(users)
+      .values({
+        clerkId: user[0].id,
+        email: user[0].emailAddresses[0].emailAddress,
+      })
   }
 
   redirect('/journal')
