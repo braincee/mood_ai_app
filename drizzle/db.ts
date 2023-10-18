ba2b749d918497ff9fff9dc10523ed44df965006
Achilles-Dev <solohag92@gmail.com>
2023-10-18 02:34:35 +0000

    Add drizzle config

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -2,9 +2,7 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless'
 import { connect } from '@planetscale/database'
 
 const config = {
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  url: process.env.DATABASE_URL,
 }
 
 const connection = connect(config)
